e7cef3f55ed6b38144b378c075eaee8f975d08c6
sauqi2010 <amsyar.nst@gmail.com>
2025-12-09 15:01:14 +0700

    [dyad] Memperbarui input gambar sketsa untuk menerima gambar dan PDF - wrote 2 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -55,10 +55,8 @@ const Login = () => {
           localization={{
             variables: {
               sign_in: {
-                link_text: { // Perbaikan: Properti ini harus berada di dalam objek link_text
-                  forgot_password: '', // Menyembunyikan tautan "Forgot your password?"
-                  no_account: '',      // Menyembunyikan tautan "Don't have an account? Sign up"
-                },
+                forgotten_password_link_text: '', // Menyembunyikan tautan "Forgot your password?"
+                link_text: '',      // Menyembunyikan tautan "Don't have an account? Sign up"
               },
               sign_up: {
                 link_text: '', // Menyembunyikan tautan "Sign in" jika pengguna beralih ke tampilan daftar
