0e7a97dc51204d006a513bf07ca9c787b0cb4a0e
sauqi2010 <amsyar.nst@gmail.com>
2025-12-11 13:07:37 +0700

    [dyad] Mengganti 'Skid Steer Loader' menjadi 'Skid Loader' - wrote 2 file(s)

--- src/data/kecamatan-kelurahan.ts
+++ src/data/kecamatan-kelurahan.ts
@@ -147,7 +147,7 @@ export const alatBeratOptions = [
   "Dump Truck",
   "Crane",
   "Wheel Loader",
-  "Skid Steer Loader",
+  "Skid Loader", // Changed from "Skid Steer Loader"
   "Asphalt Finisher",
   "Exc. Mini", 
   "Trado Mini"  
